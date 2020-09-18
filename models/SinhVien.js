c6272db64f119cdf6b39178a0dee1acfd92e13c5
hentx <hoang9961046@gmail.com>
2020-09-18 20:21:31 +0700

    Hoang_ver_3

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -7,6 +7,9 @@ var SinhVien = function(masv,tensv,email,diemtoan,diemly,diemhoa,diemrenluyen,lo
     this.diemHoa=diemhoa;
     this.diemRenLuyen=diemrenluyen;
     this.loaiSinhVien=loaisv;
+    this.xeploai = function(){
+        return "yếu";
+    }
     this.diemTrungBinh= function(){
         return((Number(this.diemToan)+Number(this.diemLy)+Number(this.diemHoa))/3);
     }    
