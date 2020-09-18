ffc86b1f46eba32b12eafb2b38a5d5a9e22ff23a
hailee994 <hailee994@gmail.com>
2020-09-18 20:15:31 +0700

    e up code nhoa a Thao lit do

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -1,14 +1,27 @@
-var SinhVien = function(masv,tensv,email,diemtoan,diemly,diemhoa,diemrenluyen,loaisv){
-    this.maSinhVien=masv;
-    this.tenSinhVien=tensv;
-    this.email=email;
-    this.diemToan=diemtoan;
-    this.diemLy=diemly;
-    this.diemHoa=diemhoa;
-    this.diemRenLuyen=diemrenluyen;
-    this.loaiSinhVien=loaisv;
-    this.diemTrungBinh= function(){
-        return((Number(this.diemToan)+Number(this.diemLy)+Number(this.diemHoa))/3);
-    }    
-
-}
+var SinhVien = function (
+  masv,
+  tensv,
+  email,
+  diemtoan,
+  diemly,
+  diemhoa,
+  diemrenluyen,
+  loaisv
+) {
+  this.maSinhVien = masv;
+  this.tenSinhVien = tensv;
+  this.email = email;
+  this.diemToan = diemtoan;
+  this.diemLy = diemly;
+  this.diemHoa = diemhoa;
+  this.diemRenLuyen = diemrenluyen;
+  this.loaiSinhVien = loaisv;
+  this.diemTrungBinh = function () {
+    return (
+      (Number(this.diemToan) + Number(this.diemLy) + Number(this.diemHoa)) / 3
+    );
+  };
+  this.xepLoai = function () {
+    return "trung binh";
+  };
+};
